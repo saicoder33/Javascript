baa616d00089e5c47a7ad23ce4976c54ed763ef8
saicoder33 <saidighe731@gmail.com>
2026-07-20 19:00:09 +0530

    Datatypes

---
+++ datatype_script.js
@@ -0,0 +1,20 @@
+// Datatypes in JavaScript
+
+//Primitives and Reference Types
+//Primitives: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+let a = "Hello"; //String
+let b = 42; //Number
+let ans = true; //Boolean
+let c = null; //Null
+
+//Reference Types: Object, Array, Function
+let a =[1,2,3]; //Array
+
+let b = {
+    name:"Sai",
+    age: 22
+}; //Object
+
+let c = function(){
+  console.log("Hello World");
+}; //Function
